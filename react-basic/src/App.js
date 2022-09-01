5ac94bd59ccb790a35185798d1fa05687b81e2e2
unohayato <mamidori1p1p@gmail.com>
2022-09-01 20:30:22 +0900

    component props

---
+++ react-basic/src/App.js
@@ -0,0 +1,13 @@
+import Article from "./components/article";
+function App() {
+  return (
+    <div>
+      <Article
+      title={"タイトル2"}
+      content={"コンテンツ2"}
+      />
+    </div>
+  );
+};
+
+export default App;
